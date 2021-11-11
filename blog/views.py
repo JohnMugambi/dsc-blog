0ec9a2508d23ea4e5d809bd537baa71f4e3cf80d
John Mugambi <mugambijm@ymail.com>
2021-11-11 20:03:45 +0300

    lesson 3 and 4

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Blog
+from .models import Blog, News
 
 # Create your views here.
 
 
 def home(request):
-    blogs = Blog.objects.all
-    #q = blogs.query
-    return HttpResponse(blogs)
+    all_news = News.objects.all()
+    all_blogs = Blog.objects.filter().order_by('-date_added')
+    context = {
+        'news': all_news,
+        'blogs': all_blogs
+    }
+    return render(request, "blog/index.html", context=context)
+
+
+def blog(request):
+    return render(request, "blog/blog.html")
+
+
+def blog_details(request):
+    return render(request, "blog/blog-details.html")
